403532af53c0aa27c45449c7ed557d0a304c6572
Michael Tu <tuzhucheng@outlook.com>
2017-11-14 03:12:27 -0500

    add script to download community corpus

---
+++ data/community-corpus/download-community-corpus.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/apache/log4j.git
+git clone https://github.com/spring-projects/spring-framework.git
+git clone https://github.com/antlr/antlr4.git
+git clone https://github.com/eclipse/jgit.git
+git clone https://github.com/itext/itextpdf.git
+
