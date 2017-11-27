992a06b8d8606caa5e20e50b8961459db9acb448
Michael Tu <tuzhucheng@outlook.com>
2017-11-27 11:26:56 -0500

    Introduce GNU parallel

--- finesand/run-all-community.sh
+++ finesand/run-all-community.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 
-set -x
+# Takes one command line arg - the number of jobs to use
 
-for d in ../data/community-corpus/*/ ; do
-    arg="${d::-1}"
-    if [[ $arg == *"log4j"* ]] ; then
-        sbtargs1="runMain finesand.PrepareData --repo $arg --branch trunk"
+repos=(../data/community-corpus/*/)
+
+for i in "${!repos[@]}" ; do
+    # strip trailing /
+    repos[$i]="${repos[$i]::-1}"
+done
+
+commands=()
+for i in "${!repos[@]}" ; do
+    repo="${repos[$i]}"
+    if [[ $repo == *"log4j"* ]] ; then
+        sbtargs1="runMain finesand.PrepareData --repo $repo --branch trunk"
     else
-        sbtargs1="runMain finesand.PrepareData --repo $arg --branch master"
+        sbtargs1="runMain finesand.PrepareData --repo $repo --branch master"
     fi
-    sbt "$sbtargs1"
+    command="$sbtargs1"
+    commands+=("$command")
+done
+
+parallel --eta -j $1 sbt ::: "${commands[@]}"
+
+#for i in "${!repos[@]}" ; do
+    #echo "${repos[$i]}"
+    #echo "${commands[$i]}"
 
     #sbtargs2="runMain finesand.BuildCounts $arg"
     #sbt "$sbtargs2"
-done
+#done
