ba550323574335076a173e8d9a59e3e123475967
Michael Tu <tuzhucheng@outlook.com>
2017-11-27 22:41:37 -0500

    Download script and repos for large corpus

---
+++ data/large-corpus/download_large_corpus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json
+import os
+from pprint import pprint
+import subprocess
+
+with open('repositories.json') as f:
+    data = json.load(f)
+    repositories = data['repositories']
+    for repo in repositories:
+        pprint(repo)
+        name = repo['url'].split('/')[-1]
+        if os.path.exists(name):
+            print('Skipping..')
+            continue
+        subprocess.call(['git', 'clone', repo['url']])
+
