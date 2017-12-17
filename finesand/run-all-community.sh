c96d16d81fa506defb4b9cd9bfde0748f793fc03
Michael Tu <tuzhucheng@outlook.com>
2017-12-17 08:32:24 +0000

    Update run-all-community script, increase mem & upload to Azure

--- finesand/run-all-community.sh
+++ finesand/run-all-community.sh
@@ -2,20 +2,20 @@
 
 # Args: [n_jobs] [optional commands to parallel]
 
-repos=(../data/community-corpus/*/)
+repoNames=(antlr4 itextpdf jgit log4j spring-framework)
+repos=("${repoNames[@]}")
 
 for i in "${!repos[@]}" ; do
-    # strip trailing /
-    repos[$i]="${repos[$i]::-1}"
+    repos[$i]="../data/community-corpus/${repoNames[$i]}"
 done
 
 commands=()
 for i in "${!repos[@]}" ; do
     repo="${repos[$i]}"
     if [[ $repo == *"log4j"* ]] ; then
-        sbtargs1="runMain finesand.PrepareData --repo $repo --branch trunk"
+        sbtargs1="runMain finesand.PrepareData --repo $repo --branch trunk --split 0.9"
     else
-        sbtargs1="runMain finesand.PrepareData --repo $repo --branch master"
+        sbtargs1="runMain finesand.PrepareData --repo $repo --branch master --split 0.9"
     fi
     command="$sbtargs1"
     commands+=("$command")
@@ -31,5 +31,21 @@ for i in "${!repos[@]}" ; do
     commands2+=("$command")
 done
 
-parallel $2 --eta -j $1 sbt ::: "${commands2[@]}"
+parallel $2 --eta -j $1 sbt ::: '-J-Xms256m' ::: '-J-Xmx8G' ::: "${commands2[@]}"
 
+for i in "${!repos[@]}" ; do
+    repo="${repos[$i]}"
+    countsDir="${repoNames[$i]}-counts"
+    countsTar="${repoNames[$i]}-counts.tar.gz"
+    cd ../data/community-corpus
+    mkdir -p $countsDir
+    cp ${repoNames[$i]}-corpus/*{p,P}art* $countsDir
+    tar -cvzf $countsTar $countsDir
+    az dls fs upload --account finesand --source-path $countsTar --destination-path /community-corpus-counts --overwrite
+    if [ $? -eq 0 ]
+    then
+        echo "Success, deleting..."
+        rm -rf ${repoNames[$i]}-corpus $countsDir $countsTar
+    fi
+    cd -
+done
